2a810f2d0e6d26e30271319691f2bf7865ece189
Shailesh Jakhaniya <ShaileshJ@viitorcloud.local>
2019-12-19 15:28:10 +0530

    Initial websocket configuration for realtime notification

---
+++ app/Events/StatusLiked.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class StatusLiked implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $username;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($username)
+    {
+        $this->username = $username;
+        $this->message  = "{$username} liked your status";
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return ['status-liked'];
+    }
+}
